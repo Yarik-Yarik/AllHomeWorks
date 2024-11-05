f65c7b36d300b94d7478ca4cd9a68ab76310fb80
Ярик-Ярик <btsov2012@mail.ru>
2024-11-05 16:35:41 +0300

    домашка 6

--- 06_lesson/pressbutton.py
+++ 06_lesson/pressbutton.py
@@ -1,33 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
-# Инициализация веб-драйвера (например, Chrome)
+# Путь к chromedriver
 driver = webdriver.Chrome()
 
+# Открыть сайт
+driver.get("http://uitestingplayground.com/ajax")
+
+# Нажать на синюю кнопку
+button = WebDriverWait(driver, 15).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, '#ajaxButton'))
+)
+button.click()
+print("Кнопка нажата, ожидаем появление зелёной плашки.")
+
+# Увеличиваем тайм-аут до 30 секунд для надежности
 try:
-    # Переход на страницу
-    driver.get("http://uitestingplayground.com/ajax")
+    green_text_element = WebDriverWait(driver, 30).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, 'p.bg-success'))
+    )
+    green_text = green_text_element.text
+    print("Зелёная плашка найдена:", green_text)
+except TimeoutException:
+    print("Не удалось найти зелёную плашку в течение времени ожидания.")
 
-    # Нажатие на синюю кнопку
-    button = driver.find_element(By.ID, "ajaxButton")
-    button.click()
-
-    # Увеличение времени ожидания до 30 секунд
-    try:
-        green_box = WebDriverWait(driver, 30).until(
-            EC.visibility_of_element_located((By.ID, "ajaxContent"))
-        )
-        text = green_box.text
-
-        # Вывод текста в консоль
-        print(text)
-
-    except TimeoutException:
-        print("Data loaded with AJAX get request.")
-
-finally:
-    # Закрытие драйвера
-    driver.quit()
+# Закрыть браузер
+driver.quit()
